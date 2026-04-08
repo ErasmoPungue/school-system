2f11ffa78f5fa1fd07faee82d279292ac4cdd245
ErasmoPungue <erasmopungue9@gmail.com>
2026-04-09 00:34:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,25 @@ require("dotenv").config();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB conectado"))
-.catch(err => console.log(err));
-
+// 🔥 ROTA TESTE (SEMPRE PRIMEIRO)
 app.get("/", (req, res) => {
   res.send("API OK");
 });
 
+// 🔥 CONEXÃO MONGO
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.log("Erro Mongo:", err));
+
+// 🔥 ROTAS
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/school", require("./routes/school"));
 
+// 🔥 PORTA (IMPORTANTE PARA RAILWAY)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
